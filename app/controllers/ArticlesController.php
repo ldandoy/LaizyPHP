123ec175999e92438edac54351d4c7b8175c5413
Laurent <laurent.Comex@sfr.fr>
2017-03-24 10:27:00 +0100

    lcomex

--- app/controllers/ArticlesController.php
+++ app/controllers/ArticlesController.php
@@ -24,10 +24,6 @@ class ArticlesController extends ApplicationController
     {
         $article = Article::findById($id);
 
-        if (empty($article)) {
-            $this->e404("Enregistrement non trouvé.");
-        }
-        
         $this->render('show', array(
             'article'   => $article,
             'titre'     => $article->titre
